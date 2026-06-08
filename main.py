92c6b1c0aa6b23f7fb167371789711844f5e7f36
gabrielsantanasilva67 <gabrielsantana4354@gmail.com>
2026-06-08 08:23:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+acertos = 0
+print("                Questões da cultura nordestina")
+print("")
+print("A literatura de cordel é um marco da cultura nordestina. Qual poeta popular é considerado o 'pai do cordel no Brasil' e adaptou a clássica obra europeia 'A Donzela Teodora'?")
+print("A- Leandro Gomes de Barros")
+print("B- Patativa do Assaré")
+print("C- João Martins de Athayde")
+print("D- Silvino Pirauá de Lima")
+pergunta1 = input(str("qual a alternativa certa?  ")).lower()
+if pergunta1 == "a":
+    acertos += 1
+elif pergunta1 == "leandro gomes de barros":
+    acertos += 1
+
+
+print(acertos)
+
